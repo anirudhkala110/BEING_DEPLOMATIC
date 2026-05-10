da07140109d8f4d11ab4ea2a19d053819b70a0d9
Anirudh Kala <122237860+anirudhkala110@users.noreply.github.com>
2026-05-10 13:09:00 +0530

    Add files via upload

---
+++ V1/src/Pages/AboutUs/AboutBase1.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const AboutBase1 = () => {
+    const [buttonNumber, setButtonNumber] = useState(1)
+    return (
+        <div className='container inter-700 pt-5'>
+            <div className='row pt-5 d-flex align-items-center justify-content-start' style={{ minHeight: '80vh' }}>
+                <div className='' style={{ fontSize: "", lineHeight: '64px' }}>
+                    <b className=' my-3 ' style={{ color: '#ff0000', fontSize: '20px' }}>WHO WE ARE</b>
+                    <div className=' text-white' style={{ maxWidth: '950px',width:'100%' }}>
+                        <p className='fs-1'>
+                            We’re here as your partners to power you to <b className='' style={{ color: '#ff0000' }}>#ChallengeTheNow</b> to build a digital future.
+                        </p>
+                        <br/>
+                        <b className='mt-4' style={{fontSize:'1.3rem',fontWeight:'300',marginTop:'100px'}}>That’s where a multi-skilled expert comes into the picture. One entity that has the solutions across platforms, genres and media.</b>
+                    </div>
+                </div>
+            </div>
+            <div className='row pb-1' style={{marginTop:''}}>
+                <div className='d-flex align-items-center justify-start px-5' style={{ borderTop: '1px solid #C6C5C5' }}>
+                    <a href='#aboutLL' className={`${buttonNumber == 1 ? 'btnActive mx-2' : 'btnNormal mx-2 '} p-3 `} onClick={() => setButtonNumber(1)} >Overview</a>
+                    {/* <a href='#values' className={`${buttonNumber == 2 ? 'btnActive mx-2' : 'btnNormal mx-2 '} py-3`} onClick={() => setButtonNumber(2)} >Our Values</a> */}
+                    {/* <a href='#groups' className={`${buttonNumber == 3 ? 'btnActive mx-2' : 'btnNormal mx-2 '} p-3`} onClick={() => setButtonNumber(3)} >Group</a> */}
+                    {/* <a href='#leadershipTeamGroup' className={`${buttonNumber == 4 ? 'btnActive mx-2' : 'btnNormal mx-2 '} p-3`} onClick={() => setButtonNumber(4)} >Meet Our Leaders</a> */}
+                    <a href='#award' className={`${buttonNumber == 5 ? 'btnActive mx-2' : 'btnNormal mx-2 '} p-3`} onClick={() => setButtonNumber(5)} >Awards & Recognition</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutBase1
---
+++ V1/src/Pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,47 @@
+import AboutBase1 from './AboutBase1'
+import AboutLL from './AboutLL'
+import './Aboutus.css'
+import React from 'react'
+import OurValues from './OurValues'
+import Groups from './Groups'
+import SpecialisedBusinessUnit from './SpecialisedBusinessUnit'
+import SettingTheStage from './SettingTheStage'
+import LeadershipTeamGroup from './LeadershipTeamGroup'
+import Awards from './Awards'
+import TeamPage from '../../Components/TeamPage'
+
+const AboutUs = () => {
+    return (
+        <div className='' style={{ minWidth: '400px' }}>
+            <div className='aboutbase1' id="top">
+                <AboutBase1 />
+            </div>
+            <div className='aboutLL bg-white py-5' id="aboutLL">
+                <AboutLL />
+            </div>
+            {/* <div className='values bg-light py-5' id="values">
+                <OurValues />
+            </div> */}
+            {/* <div className='groups bg-light py-5' id="groups">
+                <Groups />
+            </div> */}
+            {/* <div className='SpecialisedBusinessUnit py-5' style={{ background: '#034EA2' }} id="groups">
+                <SpecialisedBusinessUnit />
+            </div> */}
+            <div className='SettingTheStage bg-light py-5'>
+                <SettingTheStage />
+            </div>
+            {/* <div className='leaderTeamGroup bg-white py-5' id="leadershipTeamGroup">
+                <LeadershipTeamGroup />
+            </div> */}
+            <div className='awards bg-light py-5' id="award">
+                <Awards />
+            </div>
+            {/* <div className='contactus' id="contactUs">
+                <TeamPage />
+            </div> */}
+        </div>
+    )
+}
+
+export default AboutUs
